b7b6c1154d4381e156da5bc5927e5ecb7635b368
Jean Haro <JeanCHL98@hotmail.com>
2021-07-07 21:39:35 -0500

    Reto - Descuentos con cupones de porcentajes

--- descuentos.js
+++ descuentos.js
@@ -1,4 +1,4 @@
-// Escribiendo HTML desde JavaScript
+// Reto
 
 const precioOriginal = 120;
 const descuento = 18;
@@ -10,7 +10,7 @@ function calcularPrecioConDescuento (precio, descuento) {
     return precioConDescuento;
 }
 
-function onClickButtonPriceDiscount() {
+/* function onClickButtonPriceDiscount() {
     const inputPrice = document.getElementById('inputPrice');
     const inputDiscount = document.getElementById('inputDiscount');
     const valuePrice = inputPrice.value;
@@ -20,5 +20,31 @@ function onClickButtonPriceDiscount() {
 
     const resultP = document.getElementById('resultP');
     resultP.innerText = 'El precio con descuento son: $' + precioConDescuento;
-}
+} */
 
+/* const coupons = ['Basic', 'Expert', 'Expert+']; */
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById('inputPrice');
+    const valuePrice = inputPrice.value;
+    const selectCoupons = document.getElementById('inputCoupons');
+    const valueCoupons = selectCoupons.value;
+
+    let discount;
+
+    switch (valueCoupons) {
+        case '0': 
+            discount = 15;
+            break;
+        case '1':
+            discount = 25;
+            break;
+        case '2':
+            discount = 35;
+            break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(valuePrice, discount);
+    const resultP = document.getElementById('resultP');
+    resultP.innerText = 'El precio con descuento son: $' + precioConDescuento;
+}
