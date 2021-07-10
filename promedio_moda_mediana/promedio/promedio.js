31164cfd971d66732542badd46b9ccb23af20e3b
Jean Haro <JeanCHL98@hotmail.com>
2021-07-09 22:05:50 -0500

    Estilizando una web para calcular la media aritmética

--- promedio_moda_mediana/promedio/promedio.js
+++ promedio_moda_mediana/promedio/promedio.js
@@ -1,28 +1,42 @@
 // Calculando el promedio en JavaScript
 
-/* const lista1 = [
-    100,
-    200,
-    300,
-    500,
-]; */
+// input de indicar la cantidad de número
+let inputAmount = document.getElementById('inputAmount');
 
-// La lista lo tendría que mandar en un array
+// Añadir datos
+let containerResult = document.querySelector('.container-result .content-dividend .dividend');
+let contentDivider = document.querySelector('.container-result .divider');
+let containerResultFinish = document.querySelector('.container-result .result');
+
+// divs
+let contAmount = document.querySelector('form .content-amount');
+let contResult = document.querySelector('form .container-result');
+
+function mostrarDatos() {
+    contAmount.style.display = 'none';
+    contResult.style.display = 'flex';
+
+    for (let i = 0; i < inputAmount.value; i++) {
+        containerResult.innerHTML += `<div class="inputBox">
+                                        <input id="" type="number">
+                                    </div>`;
+    }
+    contentDivider.innerText = inputAmount.value;
+}
+
+function resultMediaAritmetica() {
+    const listaValores = document.querySelectorAll('.container-result .content-dividend .dividend input');
+    let lista = new Array;
+
+    for (let i = 0; i < listaValores.length; i++) {
+        lista.push(Number(listaValores[i].value));
+    }
+
+    containerResultFinish.innerHTML = calcularMediaAritmetica(lista);
+}
+
 function calcularMediaAritmetica (lista) {
-    /* 
-    let sumaLista = 0;
-
-    Para sumar todo los valores numéricos del Array
-    for (let i = 0; i < lista.length; i++) {
-        sumaLista = sumaLista + lista[i];
-    } 
-    */
-
-    /* reduce() -> Va a recibir una función, nosotros tenemos que enviar como argumento una función a
-    este método reduce() y nos va a permitir sumar cada uno de los elementos */
     const sumaLista = lista.reduce(
-        // Cuando JS no nos da nada, el valor por defecto de valorAcumulado será 0 porque lo colocamos ahí
-        // El nuevoElemento es el que traerá los números, y sumará con lo que se vaya acumulando en el valorAcumulado
         function (valorAcumulado = 0, nuevoElemento) {
             return valorAcumulado + nuevoElemento;
         }
@@ -34,3 +48,4 @@ function calcularMediaAritmetica (lista) {
 }
 
 calcularMediaAritmetica([1,14,151,98]); // 66
+
